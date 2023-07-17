287a15389ffb9679853c925cb90dccd0adafd0c5
Carter Parks <cparks@controlbyweb.com>
2023-07-17 10:13:34 -0600

    Efficiency and pruning

--- d29e.hs
+++ d29e.hs
@@ -5,10 +5,12 @@ main :: IO ()
 main = do
   args <- getArgs
   file <- readFile $ head args
-  putStrLn (intercalate "\n" (d29e file))
+  disp . d29e . lines $ file
 
-d29e :: String -> [String]
-d29e = stab . shrt . concat . comb . (\xs -> map init xs) . lines
+disp xs = putStr $ intercalate "\n" xs
+
+d29e :: [String] -> [String]
+d29e = stab . comb
 
 -- strings and their abbreviations
 stab :: [String] -> [String]
@@ -18,26 +20,14 @@ stab xs = zipWith (\a b -> a ++ " " ++ b) xs (map abbr xs)
 abbr :: String -> String
 abbr x = [head x] ++ (show (length x - 2)) ++ [last x]
 
--- shortest words, alphabetically
-shrt :: [String] -> [String]
-shrt xs = sortBy (\a b -> compare (length a) (length b)) (sort xs)
+-- sorted, grouped, unique words
+comb :: [String] -> [String]
+comb xs = map (\(_,_,_,x) -> x) $ concat $ uniq $ sort $ map lab $ xs
 
--- first-last letter combo, find unique middle letter counts
-comb :: [String] -> [[String]]
-comb xs = [uniq $ fl a b xs | a <- ['a'..'z'], b <- ['a'..'z']]
+-- unique words
+uniq :: [(Int, Char, Char, String)] -> [[(Int, Char, Char, String)]]
+uniq xs = filter (\xs -> length xs == 1) $ groupBy (\(a,b,c,_) (d,e,f,_) -> (a,b,c) == (d,e,f)) $ xs
 
--- words with first-last letter combo
-fl :: Char -> Char -> [String] -> [String]
-fl a b xs = [x | x <- xs, a == head x, b == last x]
-
--- words with unique middle letter counts
-uniq :: [String] -> [String]
-uniq xs = filter (\x -> (midc x xs) == 1) xs
-
--- how many times the middle count appears
-midc :: String -> [String] -> Int
-midc x xs = (length . filter (\y -> mid x == mid y)) xs
-
--- middle count
-mid :: String -> Int
-mid x = (length x) - 2
+-- label string with leng first last
+lab :: String -> (Int, Char, Char, String)
+lab x = (length x, head x, last x, x)
